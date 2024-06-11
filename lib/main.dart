cda514d9ef767907053356d00530ee63c82cb02a
Safi32 <138612632+Safi32@users.noreply.github.com>
2024-06-11 15:50:20 +0500

    get

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: First(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class First extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GeekforGeeks"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Container(
+          child: ElevatedButton(
+              child: Text("Go to next screen"),
+              onPressed: () {
+                Get.to(Second());
+              }),
+        ),
+      ),
+    );
+  }
+}
+
+class Second extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GeekforGeeks"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Container(
+          child: ElevatedButton(
+              child: Text("Go to first screen"), onPressed: null),
+        ),
+      ),
+    );
+  }
+}
